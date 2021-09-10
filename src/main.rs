453eb0caf6e33433487a13a3456820678dfb1e45
Jeff Shen <jshen2014@hotmail.com>
2021-09-10 14:30:16 -0400

    weighted average over all measurement datasets

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use compute::prelude::{argmin, interp1d_linear_unchecked, linspace, ExtrapolationMode, Vector};
+use arrayvec::ArrayVec;
+use compute::prelude::{
+    argmin, interp1d_linear_unchecked, linspace, Dot, ExtrapolationMode, Vector,
+};
 use csv::ReaderBuilder;
 use dragonfly_rs::calibration::{PNEFLUX, PNEFLUX_NII, PNETILT};
 use rayon::prelude::*;
@@ -11,52 +14,79 @@ pub struct PNRecord {
 }
 
 fn main() {
-    let rdr = ReaderBuilder::new()
-        .delimiter(b' ')
-        .has_headers(false)
-        .from_path("/home/js/programs/dragonfly-rs/data/PNeMeasurements/measure_source_303.txt")
-        .expect("Could not open file as csv");
+    let bests = (1..4)
+        .map(|i| {
+            let rdr = ReaderBuilder::new()
+                .delimiter(b' ')
+                .has_headers(false)
+                .from_path(
+                    format!(
+                    "/home/js/programs/dragonfly-rs/data/PNeMeasurements/measure_source_30{}.txt",
+                    i
+                )
+                    .as_str(),
+                )
+                .expect("Could not open file as csv");
 
-    let data = rdr
-        .into_deserialize()
-        .map(|x| x.expect("Could not deserialize field."))
-        .collect::<Vec<PNRecord>>();
+            let data = rdr
+                .into_deserialize()
+                .map(|x| x.expect("Could not deserialize field."))
+                .collect::<Vec<PNRecord>>();
 
-    let pnedatatilt = data.iter().map(|x| x.angle).collect::<Vector>();
-    let pnedataflux = data.iter().map(|x| x.flux).collect::<Vector>();
-    let pnedatafluxnorm = &pnedataflux / pnedataflux.max();
+            let pnedatatilt = data.iter().map(|x| x.angle).collect::<Vector>();
+            let pnedataflux = data.iter().map(|x| x.flux).collect::<Vector>();
+            let pnedatafluxnorm = &pnedataflux / pnedataflux.max();
 
-    let fractions = linspace(0., 0.5, 100);
-    let shifts = linspace(-10., 10., 500);
+            let fractions = linspace(0., 0.5, 100);
+            let shifts = linspace(-10., 10., 500);
 
-    let mut results = fractions
-        .par_iter()
-        .map(|&frac| {
-            let totalflux = PNEFLUX
+            let mut result = fractions
+                .par_iter()
+                .map(|&frac| {
+                    let totalflux = PNEFLUX
+                        .iter()
+                        .zip(PNEFLUX_NII)
+                        .map(|(x, y)| x + frac * y)
+                        .collect::<Vector>();
+                    let totalfluxnorm = &totalflux / totalflux.max();
+                    let shift_interp = |x: &[f64]| {
+                        interp1d_linear_unchecked(
+                            &PNETILT,
+                            &totalfluxnorm,
+                            x,
+                            ExtrapolationMode::Fill(0.),
+                        )
+                    };
+                    let residual = |s: f64| {
+                        (shift_interp(&(&pnedatatilt - s)) - &pnedatafluxnorm)
+                            .powi(2)
+                            .sum()
+                    };
+                    let res_wrt_shifts =
+                        shifts.par_iter().map(|&x| residual(x)).collect::<Vector>();
+                    let min_idx = argmin(&res_wrt_shifts);
+
+                    (frac, shifts[min_idx], res_wrt_shifts[min_idx])
+                })
+                .collect::<Vec<_>>();
+
+            let best = result
                 .iter()
-                .zip(PNEFLUX_NII)
-                .map(|(x, y)| x + frac * y)
-                .collect::<Vector>();
-            let totalfluxnorm = &totalflux / totalflux.max();
-            let shift_interp = |x: &[f64]| {
-                interp1d_linear_unchecked(&PNETILT, &totalfluxnorm, x, ExtrapolationMode::Fill(0.))
-            };
-            let residual = |s: f64| {
-                (shift_interp(&(&pnedatatilt - s)) - &pnedatafluxnorm)
-                    .powi(2)
-                    .sum()
-            };
-            let res_wrt_shifts = shifts.par_iter().map(|&x| residual(x)).collect::<Vector>();
-            let min_idx = argmin(&res_wrt_shifts);
+                .min_by(|a, b| a.2.partial_cmp(&b.2).unwrap())
+                .unwrap()
+                .to_owned();
 
-            (frac, shifts[min_idx], res_wrt_shifts[min_idx])
+            best
         })
-        .collect::<Vec<_>>();
+        .collect::<ArrayVec<_, 3>>();
 
-    let best = results
-        .iter()
-        .min_by(|a, b| a.2.partial_cmp(&b.2).unwrap())
-        .unwrap();
+    let strengths = Vector::from([bests[0].0, bests[1].0, bests[2].0]);
+    let shifts = Vector::from([bests[0].1, bests[1].1, bests[2].1]);
+    let weights = 1. / Vector::from([bests[0].2, bests[1].2, bests[2].2]).powi(2);
 
-    println!("{:?}", best);
+    println!(
+        "Nitrogen relative strength: {}",
+        weights.dot(strengths) / weights.sum(),
+    );
+    println!("Best angle shift: {}", weights.dot(shifts) / weights.sum());
 }
